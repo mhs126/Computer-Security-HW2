8a102d539f0db9369218f783b96fe6600c1bf02a
Michael Silverman <44821540+mhs126@users.noreply.github.com>
2019-10-13 01:26:06 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int searchForString(char *fileName, char *string){
+	FILE *file;
+	char buffer[1024];
+	
+	file = fopen(fileName, "r");
+	fgets(buffer, 1024, file);
+	if(strstr(buffer, string)!=NULL){
+		return 1;
+	}
+	return 0;
+}
+
+int main(){
+	char *checkString;
+	checkString = "I want to learn PE file format!";
+	char *fileName;
+	FILE *file;
+	fileName = "PE-1.txt";
+	char data[1024] = "I want to learn PE file format!";
+	
+	if(access(fileName, F_OK) != -1){
+		if(searchForString(fileName, checkString) == 1){
+		    printf("File and Line found");
+			return 0;
+		} 
+		else{
+			file = fopen(fileName, "a");
+			fprintf(file, " %s", checkString);
+			fclose(file);
+			printf("File found but line not found");
+			return 0; 
+		}
+	}
+	else{
+		file = fopen(fileName, "w");
+		if(file == NULL){
+			exit(EXIT_FAILURE);
+		}
+		fputs(data, file);
+		fclose(file);
+		printf("No file found");
+		return 0;
+	}
+}
+	
